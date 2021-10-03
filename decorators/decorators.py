63a55ce893c088191a07590431895ba5524d669e
Oliver Otchere <oliverotchere4@gmail.com>
2021-10-03 12:29:18 +0000

    added access-denied func

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+
+from django.shortcuts import render
 
 
 def administrator_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.role != "Administrator":
-            return HttpResponse("You are not authorized to see this page")
+
+            return render(request, "access-denied.html")
         else:
             return view_func(request, *args, **kwargs)
     return wrapper_func
